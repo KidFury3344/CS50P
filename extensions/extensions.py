db41ed7655a31214037c67672f85ff1417c2d414
Ebrahim Tayabali <ebrahimhakimuddin@gmail.com>
2024-06-30 01:13:07 +0300

    "Added gitignore"

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,17 +1,17 @@
 file_name = input("File Name: ").lower().strip()
 dot_index = file_name.find(".")
 
-if file_name[dot_index:] == ".gif":
+if file_name[-1:dot_index] == "gif":
     print("image/gif")
-elif (file_name[dot_index:] == ".jpg") or (file_name[dot_index:] == ".jpeg"):
+elif (file_name[-1:dot_index] == "jpg") or (file_name[-1:dot_index] == "jpeg"):
     print("image/jpeg")
-elif file_name[dot_index:] == ".png":
+elif file_name[-1:dot_index] == "png":
     print("image/png")
-elif file_name[dot_index:] == ".pdf":
+elif file_name[-1:dot_index] == "pdf":
     print("application/pdf")
-elif file_name[dot_index:] == ".txt":
+elif file_name[-1:dot_index] == "txt":
     print("text/plain")
-elif file_name[dot_index:] == ".zip":
+elif file_name[-1:dot_index] == "zip":
     print("application/zip")
 else:
     print("application/octet-stream")
